f4a6b60774334ae0f6d804b9b985c51d898e04ef
MPote07 <105569325+MPote07@users.noreply.github.com>
2024-11-12 21:32:02 -0300

    Add files via upload

---
+++ SWII6/TP03/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace TP03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+// Lucas Vieira Dos Santos Martins CB3020223 & Miguel da Silva Pataro CB3021564
---
+++ SWII6/TP03/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP03.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O preço é obrigatório.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+        public decimal Preco { get; set; }
+
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "A quantidade é obrigatória.")]
+        [Range(0, int.MaxValue, ErrorMessage = "A quantidade não pode ser negativa.")]
+        public int Quantidade { get; set; }
+    }
+}
+// Lucas Veira Dos Santos Martins CB3020223 & Miguel da Silva Pataro CB3021564
